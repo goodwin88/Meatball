8bab1e7da092ecc2d3d6efb80fa3560b971b61d8
Chris Goodwin <chris@goodwinillustration.com>
2019-06-20 22:43:46 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1561084930,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bd-sheet0.png",
+		"images/uianchor-sheet0.png",
+		"images/portrait-sheet0.png",
+		"images/portrait-sheet1.png",
+		"images/portrait-sheet2.png",
+		"images/uianchor2-sheet0.png",
+		"images/uianchor3-sheet0.png",
+		"images/uicursor-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
